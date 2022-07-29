531f0663982ab5b3daed6044ece21d944f8710a4
Riddhi Jain <jainriddhi2110@gmail.com>
2022-07-29 18:20:52 +0530

    Add project files.

---
+++ Application.Entitiess/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entities
+{
+    internal class Doctor
+    {
+        public int Id { get; set; }
+        public string D_name { get; set; }
+        public int Dept_id { get; set; }
+
+    }
+}
---
+++ Application.Entitiess/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entitiess
+{
+    internal class Patient
+    {
+        public int Id { get; set; }
+        public string P_name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int Dept_id { get; set; }
+        public int Dr_id { get; set; }
+        public bool Is_assigned {get; set;}
+        public int Assigned_to_dr_id { get; set; }
+
+    }
+}
